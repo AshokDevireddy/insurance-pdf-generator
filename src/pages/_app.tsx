9a8dab27ff0ac0b37ed092b36b086b87c07dfbb6
Ashok Devireddy <ashokrd@berkeley.edu>
2025-02-07 14:57:15 -0800

    enable analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next";
 import type { AppProps } from "next/app";
 import Banner from "@/components/Banner";
 
